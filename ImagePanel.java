dc8ccab7f630e171523e38389d997d158ba387ac
mmcintyre03 <150456556+mmcintyre03@users.noreply.github.com>
2024-01-11 11:41:42 -0500

    Update ImagePanel.java

--- ImagePanel.java
+++ ImagePanel.java
@@ -5,18 +5,19 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
+//This class allows the images to be placed on the background images 
 class ImagePanel extends JPanel{
-  private Image img;
+  private Image img; 
 
   public ImagePanel(String img){
-    this(new ImageIcon(img).getImage());
+    this(new ImageIcon(img).getImage()); //getting the image that is being passed through 
   }
 
-  public ImagePanel(Image img){
-    this.img = img;
-    Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
-    setPreferredSize(size);
-    setMinimumSize(size);
+  public ImagePanel(Image img){ 
+    this.img = img; 
+    Dimension size = new Dimension(img.getWidth(null), img.getHeight(null)); //sets the image width and height to null 
+    setPreferredSize(size); //sets the size to be the inputed size 
+    setMinimumSize(size); 
     setMaximumSize(size);
     setSize(size);
     setLayout(null);
